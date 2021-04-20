343dd1537f7e6c11963457bfd575277251dc0317
Catherine Martin <cathysue86@gmail.com>
2021-04-20 11:28:35 -0700

    adds pure recursion version to collectOddValues

--- Recursion/collectOddValues.js
+++ Recursion/collectOddValues.js
@@ -1,9 +1,12 @@
 // Helper Method Recursion => outer fx (that's not recursive) calls inner fx that is recursive
+// VS
+// Pure Recursion
 
 // fx that finds all odd values in an array, then return a new array with the odd values only
     // can #s be negative?
     // are values only integers?
 
+// HELPER METHOD
 // OUTER fx
 function collectOddValues(arr) {
     // declare this outside of recursive fx so it doesn't get reset each time make recursive call
@@ -24,4 +27,20 @@ function collectOddValues(arr) {
     return result
 }
 
-collectOddValues([1,2,3,4,5,6,7,8])
+// PURE RECURSION
+function collectOddValues(arr) {
+    // this array will be reset to empty w/each recursive call so will need to use concat to combine all the array result arrays together
+    let result = [];
+    // Base case
+    if (arr.length === 0) return result;
+    // if 1st value in input array is odd, add it to result array
+    if (arr[0] % 2 !== 0) {
+        result.push(arr[0]);
+    };
+    // make recursive call on the rest of the input array except for first value
+      // concat that returned array from this recursive call to the previous result array and return final result
+    result = result.concat(collectOddValues(arr.slice(1)))
+    return result;
+}
+
+collectOddValues([1,2,3,4,5,6,7,8]);
