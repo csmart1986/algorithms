1cb90fa0d6a3da5b051fb6bdc686d52d2122a264
Catherine Martin <cathysue86@gmail.com>
2021-04-27 11:12:39 -0700

    adds recursive solution to palindromeCheck

--- Recursion/palindromeCheck.js
+++ Recursion/palindromeCheck.js
@@ -2,12 +2,14 @@
   // case insensitive?  yes
   // can there be numbers, spaces, special characters?  yes
   // what if string is empty or 1 character long?  return true
+  // can use string methods? yes
 
+// ITERATIVE w/POINTERS
 function palindromeCheck(str) {
     // put pointers on either end of the string
     let start = 0;
     let end = str.length - 1;
-    // loop for as long as start and end pointers don't pass each other
+    // loop for as long as start and end pointers don't pass each other (until you reach middle of string)
     while (start <= end) {
         // convert letters at both pointers to lowercase so they're the same casing
         if (str[start].toLowerCase() === str[end].toLowerCase()) {
@@ -21,6 +23,24 @@ function palindromeCheck(str) {
     // have matched all the characters in the string -> is a palindrome
     return true;
 }
+
+// RECURSIVE 
+function palindromeCheck(str) {
+    // base case -> if empty string or length of 1, it is a palindrome
+    if (str.length <= 1) return true;
+  
+    let firstIdx = 0
+    let lastIdx = str.length - 1;
+    // if first and last letters don't match, not a palindrome
+    if (str[firstIdx].toLowerCase() !== str[lastIdx].toLowerCase()) {
+      return false;
+    }
+    // make recursive call on string that has its first and last letters sliced off
+    else return palindromeCheck(str.slice(1,-1))
+}
+
+// time complexity O(n) where n is string length and must recurse through string n/2 times in order to return false
+// space complexity O(n) b/c create n/2 additional calls on callstack when we slice off first/last chars of string
 
 palindromeCheck('car') // false
 palindromeCheck('!? 100 ABCcba 001 ?!') // true
