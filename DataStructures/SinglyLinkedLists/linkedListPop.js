0bdb5df672df979ea4ce5d8264b3f5bb30db5e55
Catherine Martin <cathysue86@gmail.com>
2021-05-06 21:43:02 -0700

    edits linkedListPop.js

--- DataStructures/SinglyLinkedLists/linkedListPop.js
+++ DataStructures/SinglyLinkedLists/linkedListPop.js
@@ -23,13 +23,13 @@ class SinglyLinkedList {
   
         // if it's an empty list, set the head & tail to be the newly created node
         if (!this.head) {
-        this.head = newNode;
-        this.tail = this.head;
+          this.head = newNode;
+          this.tail = this.head;
         }
         // otherwise, take the current tail, add new node onto the end of it, then move the tail marker to point to the new node
         else {
-        this.tail.next = newNode;
-        this.tail = newNode;
+          this.tail.next = newNode;
+          this.tail = newNode;
         }
         // increment length by 1
         this.length++;
@@ -59,6 +59,7 @@ class SinglyLinkedList {
       this.tail = newTail;
       // set the new tail's next node to be null (nothing comes after it and connection to old tail has been severed)
       this.tail.next = null
+      // decrement length by 1
       this.length--;
 
       // if list is now empty, then reset head and tail to be null
