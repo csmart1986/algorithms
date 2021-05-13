6f540d9e8c062718e22591769f781185bead3fab
Catherine Martin <cathysue86@gmail.com>
2021-05-13 08:48:27 -0700

    adds linkedListSet

---
+++ DataStructures/DoublyLinkedLists/linkedListSet.js
@@ -0,0 +1,100 @@
+// SET => replacing the value of a node in DoublyLinkedList
+
+// define a Node class
+class Node {
+    constructor(val) {
+      this.val = val;
+      // references the next node (initially null b/c nothing that comes after it)
+      this.next = null;  
+      // references the previous node initially null b/c nothing comes before it)
+      this.prev = null;
+    }
+  }
+// define a DoublyLinkedList class
+class DoublyLinkedList {
+    constructor() {
+        this.length = 0,
+        this.head = null,
+        this.tail = null
+    }
+
+    // create PUSH instance method
+    push(val) {
+        // create a new node instance where the value of the node is what is being passed to push method
+        let newNode = new Node(val);
+  
+        // if it's an empty list, set the head & tail to be the newly created node
+        if (!this.head) {
+          this.head = newNode;
+          this.tail = newNode;
+        }
+        // otherwise, take the current tail, add new node onto the end of it,  set prev node to be the current tail, then move the tail marker to point to the new node
+        else {
+          this.tail.next = newNode;
+          newNode.prev = this.tail;
+          this.tail = newNode;
+        }
+        // increment length by 1
+        this.length++;
+        // return the linked list
+        return this;
+    }
+
+    // create GET instance method
+    get(idx) {
+      // invalid idx
+      if (idx < 0 || idx >= this.length) return null;
+      let counter;
+      let current;
+      // position we're accessing is in 1st half of list
+      if (idx <= this.length/2) {
+        // set current node to the head
+        current = this.head;
+        counter = 0;
+        while (counter < idx) {
+          // traverse from front of list
+          current = current.next;
+          counter++;
+        }
+      }
+      // position is in 2nd half of list
+      else {
+        // set current node to the tail
+        current = this.tail;
+        counter = this.length - 1;
+        while (counter > idx) {
+          // traverse backwards from end of list
+          current = current.prev;
+          counter--;
+        }
+      }
+      return current;
+    }
+
+    // create SET instance method
+    set(idx, val) {
+      let targetNode = this.get(idx);
+      // if you found node by calling .get(), reassign its value
+      if (targetNode) {
+        targetNode.val = val;
+        return true;
+      }
+      // didn't find the node
+      return false;
+    }
+}
+  
+// initially, when we make a new list instance, it is empty
+let list = new DoublyLinkedList()
+
+// add on this value to end of the list
+list.push('HELLO')
+list.push('GOODBYE')
+list.push('!')
+
+// set value at specific position
+list.set(1, 'ADIOS')
+list.get(1)
+
+// set at a invalid position
+// list.set(-1, 'ADIOS')
