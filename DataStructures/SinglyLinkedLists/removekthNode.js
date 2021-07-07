c6cd5a5706d0512222f898a8be2e808d466e6eae
Catherine Martin <cathysue86@gmail.com>
2021-07-06 21:34:48 -0700

    adds removekthNOde.js to singly linked lists

---
+++ DataStructures/SinglyLinkedLists/removekthNode.js
@@ -0,0 +1,14 @@
+// Fx that takes in the head of a Singly linked list and an integer k and removes the kth node from the end of the list
+  // removal should be done IN PLACE
+
+// the input head of the linked list should remain the head of the linked list after the removal is done, even if the head is the node that's supposed to be removed
+  // if the head is the node that's supposed to be removed, your function should simply mutate its value and next pointer
+
+// fx DOES NOT need to return anything
+
+// can assume that the input linked list will always have at least 2 nodes and , more specifically, at least k node
+
+// head = 0 1 2 3 4 5 6 7 8 9 (input is head node with value 0)
+// k = 4
+// output = 0 1 2 3 4 5 7 8 9
+  // 4th node form the end of list is removed (node with value 6)
